cdac0af1097d0671f46a8a1e99dae6398dab45ea
caramucho <zl.iafag@gmail.com>
2022-04-26 00:02:31 +0900

    exception

--- tutorial/tutorial/spiders/shein_spider.py
+++ tutorial/tutorial/spiders/shein_spider.py
@@ -14,20 +14,24 @@ class SheinSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             # "https://jp.shein.com/Honeyspot-Solid-Drop-Shoulder-Oversized-Blouse-p-10209809-cat-1733.html"
-            "https://jp.shein.com/DAZY-Bishop-Sleeve-Sheer-Blouse-p-9973864-cat-1733.html"
+            # "https://jp.shein.com/DAZY-Bishop-Sleeve-Sheer-Blouse-p-9973864-cat-1733.html"
             # "https://uiuret.hatenablog.com/entry/2022/04/23/125305"
+            "https://jp.shein.com/Rhinestone-Rib-knit-Sports-Brief-p-10283098-cat-3056.html"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
             # yield SeleniumRequest(url=url, callback=self.parse)
 
     def parse(self, response):
-        shein_script = response.css("script")[12].get()
-        ssdata = json.loads(shein_script.split("productIntroData:")[
-                            1].split(",\n        abt: ")[0])
-        goods_name = ssdata["detail"]["goods_name"]
-        skc = ssdata["getSeriesAndBrand"]["skc"]
-        print(goods_name, skc)
+        try:
+            shein_script = response.css("script")[12].get()
+            ssdata = json.loads(shein_script.split("productIntroData:")[
+                                1].split(",\n        abt: ")[0])
+            goods_name = ssdata["detail"]["goods_name"]
+            skc = ssdata["getSeriesAndBrand"]["skc"]
+            print(goods_name, skc)
+        except Exception:
+            print("Can not load goods")
         # title = response.css("h1.product-intro__head-name::text").get()
         # print(title)
         # print(response.selector.xpath('//title/@text'))
