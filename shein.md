0295baa7f8526a90914f36351c4ef30fab15a92f
caramucho <zl.iafag@gmail.com>
2022-04-29 11:57:54 +0900

    collect item urls

--- shein.md
+++ shein.md
@@ -2,6 +2,13 @@
 ### Run spider
 ```
 scrapy crawl shein
+scrapy crawl shein_items
+```
+
+
+### Extracting data using the Scrapy shell
+```
+scrapy shell "https://jp.shein.com/Women-Blouses-c-1733.html"
 ```
 ## crawl women-clothing-recommended to collect the items 
 https://jp.shein.com/Clothing-c-2030.html?ici=jpen_tab01navbar03&scici=navbar_WomenHomePage~~tab01navbar03~~3~~webLink~~~~0&src_module=topcat&src_tab_page_id=page_select_class1650511045249&src_identifier=fc%3DWomen%60sc%3DCLOTHING%60tc%3D0%60oc%3D0%60ps%3Dtab01navbar03%60jc%3Durl_https%253A%252F%252Fjp.shein.com%252FClothing-c-2030.html&srctype=category&userpath=category%3ECLOTHING
@@ -29,3 +36,18 @@ shein_script = response.css("script")[12].get()
 json.loads(shein_script.split("productIntroData:")[
                                 1].split(",\n        abt: ")[0])
 ```
+
+# find items
+items
+https://jp.shein.com/Women-Blouses-c-1733.html
+## wrapper class
+S-product-item__wrapper
+```
+response.css("div.S-product-item__wrapper")
+```
+## get all links in wrapper class
+```
+response.css("div.S-product-item__wrapper a::attr(href)").getall()
+```
+## next page 
+https://jp.shein.com/Women-Blouses-c-1733.html?page=2
---
+++ tutorial/tutorial/spiders/items_spider.py
@@ -0,0 +1,55 @@
+import scrapy
+import json
+import pymongo
+# from shutil import which
+# from scrapy_selenium import SeleniumRequest
+
+# SELENIUM_DRIVER_NAME = 'edge'
+# SELENIUM_DRIVER_EXECUTABLE_PATH = which('msedgedriver')
+# SELENIUM_DRIVER_ARGUMENTS = ['--headless']
+
+
+class SheinSpider(scrapy.Spider):
+    name = "shein_items"
+
+    def load_db(self):
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["mongotest"]
+        self.col = db["urls"]
+
+    def start_requests(self):
+        self.load_db()
+        urls = [
+            "https://jp.shein.com/Women-Blouses-c-1733.html"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+            # yield SeleniumRequest(url=url, callback=self.parse)
+
+    def parse(self, response):
+        try:
+            item_urls = response.css(
+                "div.S-product-item__wrapper a::attr(href)").getall()
+            print(item_urls)
+        except Exception:
+            print("Can not load goods")
+        try:
+            item_urls_json = [{"url": iu} for iu in item_urls]
+            res = self.col.insert_many(item_urls_json)
+            print("inserted: ", res.inserted_ids)
+        except Exception:
+            print("bulk write error")
+
+            # title = response.css("h1.product-intro__head-name::text").get()
+            # print(title)
+            # print(response.selector.xpath('//title/@text'))
+            # page = response.url.split("/")[-1]
+            # filename = f'shein-{page}.html'
+            # with open(filename, 'wb') as f:
+            #     f.write(response.body)
+            # for title in response.css("h2 a"):
+            #     yield {
+            #         "title": title.css("a::text").get(),
+            #         "url": title.css("a::attr(href)").get(),
+            #     }
+            # self.log(f'Saved file {filename}')
